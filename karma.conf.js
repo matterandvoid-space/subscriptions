f859a37b094ad1fd4ee0736725aacdb5286ff718
Daniel Vingo <dan@danvingo.com>
2022-05-29 21:50:16 -0400

    working on ci tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,30 @@
 module.exports = function (config) {
-  var root = 'run/compiled/karma/test' // same as :output-dir
-  var junitOutputDir = process.env.CIRCLE_TEST_REPORTS || "run/compiled/karma/test/junit"
-  if (process.env.CANARY_CLOJURESCRIPT_VERSION !== undefined) {
-      var browsers = ['PhantomJS']
-  } else {
-      var browsers = ['ChromeHeadless']
-  }
-
+  var root = 'target' // same as :output-dir in shadow-cljs.edn
   config.set({
     frameworks: ['cljs-test'],
-    browsers: browsers,
+    browsers: ['ChromeHeadless'],
     basePath: './',
     files: [
-      root + '/test.js'
+      root + '/ci.js'
     ],
     plugins: [
         'karma-cljs-test',
-        'karma-phantomjs-launcher',
         'karma-chrome-launcher',
-        'karma-junit-reporter'
+        // 'karma-junit-reporter'
     ],
     colors: true,
     logLevel: config.LOG_INFO,
+
     client: {
       args: ['shadow.test.karma.init'],
       singleRun: true
     },
 
     // the default configuration
-    junitReporter: {
-      outputDir: junitOutputDir + '/karma', // results will be saved as outputDir/browserName.xml
-      outputFile: undefined, // if included, results will be saved as outputDir/browserName/outputFile
-      suite: '' // suite will become the package name attribute in xml testsuite element
-    }
+    // junitReporter: {
+    //   outputDir: junitOutputDir + '/karma', // results will be saved as outputDir/browserName.xml
+    //   outputFile: undefined, // if included, results will be saved as outputDir/browserName/outputFile
+    //   suite: '' // suite will become the package name attribute in xml testsuite element
+    // }
   })
 }
